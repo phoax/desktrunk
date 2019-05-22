79abb623ea8c47e94a5d0d288b27fe9df781b9f4
Raphaël <raphael.pralat@gmail.com>
2019-05-23 00:45:26 +0200

    bug fix after first test

--- sketch/src/main.cpp
+++ sketch/src/main.cpp
@@ -1,34 +1,82 @@
 #include <Arduino.h>
+#include <Desktrunk.h>
+#include <NFC_Factorim.h>
 
-#include "BluetoothSerial.h"
+#include <Thread.h>
+#include <ThreadController.h>
 
-BluetoothSerial ESP_BT;
+const char * secret = "sesame";
 
-int incoming;
+#define INTERRUPT_PIN (1)
+
+#define STEPPER_IN1 (15)
+#define STEPPER_IN2 (2)
+#define STEPPER_IN3 (0)
+#define STEPPER_IN4 (4)
+
+#define PN532_SCK  (18)
+#define PN532_MOSI (23)
+#define PN532_MISO (19)
+#define PN532_SS   (5)
+
+#define LED_RED (27)
+#define LED_GREEN (26)
+#define LED_BLUE (25)
+
+Desktrunk desktrunk;
+
+NFC_Factorim nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
+
+/**
+ * Callback for nfc
+ */
+void nfcCallback(){
+  char * data = nfc.readPassive();
+
+  if (data) {
+    if (desktrunk.checkAccess(data)) {
+      if (desktrunk.trunk.closed == true) {
+        Serial.println("Access granted");
+        desktrunk.openingTrunk();
+      } else {
+        Serial.println("Already opened");
+        desktrunk.ledSuccess();
+      }
+    }
+  }
+}
+
+/**
+ * Closing front
+ */
+void closingFront() {
+  if (desktrunk.trunk.closed) {
+    desktrunk.openingTrunk();
+  } else {
+    desktrunk.closingTrunk();
+  }
+}
 
 void setup(void) {
   Serial.begin(9600);
+  Serial.println("Desktrunk starting");
 
-  ESP_BT.begin("ESP32_LED_Control");
-  Serial.println("Bluetooth Device is Ready to Pair");
+  nfc.begin();
+  nfc.nfcInfo();
+  nfc.SAMConfig();
+
+  desktrunk.init();
+  desktrunk.setSecret(secret);
+  desktrunk.setStepper(STEPPER_IN1, STEPPER_IN2, STEPPER_IN3, STEPPER_IN4);
+  desktrunk.setLed(LED_RED, LED_GREEN, LED_BLUE);
+
+  pinMode(INTERRUPT_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), closingFront, CHANGE);
+
+  desktrunk.ledStart();
 }
 
 void loop(void) {
-  if (ESP_BT.available()) //Check if we receive anything from Bluetooth
-  {
-    incoming = ESP_BT.read(); //Read what we recevive 
-    Serial.print("Received:");
-    
-    Serial.println(incoming);
-
-    if (incoming == 49) {
-      ESP_BT.println("LED turned ON");
-    }
-        
-    if (incoming == 48) {
-      ESP_BT.println("LED turned OFF");
-    }
-  }
-  ESP_BT.println("Bluuuuuuuuuuue");
-  delay(1000);
+  nfcCallback();
+  desktrunk.check();
 }
